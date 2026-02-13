59d10606d5c32331081cadc7954de43f64d9be5f
Shoaib Ahmad <shoaibkhan19815192118114@gmail.com>
2026-02-13 11:57:42 +0530

    Week 4 v_1

---
+++ Week_4/w4q5.py
@@ -0,0 +1,9 @@
+# Write a program to test password strength based on length, complexity, 
+# characters, and patterns. Use it to evaluate common passwords.
+
+
+'''should have atleast 8 character, 16 is a safer choice'''
+'''uppercase, lowercase,special character, Numbers'''
+
+s = input("Enter Password : ")
+
